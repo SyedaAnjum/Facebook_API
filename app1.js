fc0d2fe7f0eda6d29c72d3087db3f98df4f7b74f
SyedaAnjum <anjumsyeda7@gmail.com>
2017-11-12 17:06:11 +0530

    changes made in js

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-var myFacebookToken ='EAACEdEose0cBAPZBKmyQXXi189U8RCsZAyNgQC3UlGLaOzCj6VElvw4EGZAyNir5KPDVQbJwYGYuJ01xCFnKBKgn2kU3s9FbBnzYz00OXwrYsSTySovJIbLHc9CJxdtR9K0CwPVKfJQvVQOwtHNn09VBR7qMyHQ4tBDQusahIoZBiaZCZAZCvE7LQATaFV9o4VYVbu1W19vqAZDZD';
+var myFacebookToken ='EAACEdEose0cBANstm9OeKmIwZCdzq9ZA5LL9ScMMhgZBln7bDTEkQgKzO2Tx30JTIRraAHDHByCkZA5ERu7APmCnnpJvkuAMhS1gNDeZC8hG4xXU8Ot5KLliMUfEdZB4kVO2UdST90SQXSU3yhdlHXnublkkl79jhZC5KdJjloIvZAg1WuZCZBZC4cFARHC3WMUWQm90AjZBpYM01AZDZD';
 
 function getFacebookInfo() {
 
@@ -9,70 +9,69 @@ function getFacebookInfo() {
           success: function(response){
                console.log(response);
                console.log(typeof(response));
-                $("#myFirstName").text(response.first_name);
+                    $("#myFirstName").text(response.first_name);
                     $("#myLastName").text(response.last_name);
                     $("#myFbName").text(response.name);
-                $("#myBirthday").text(response.birthday);
-               $("#myProfileId").html('<a target="blank" href="https://facebook.com/'+response.id+'">https://facebook.com/'+response.id+'</a>');
+                    $("#myBirthday").text(response.birthday);
+                    $("#myProfileId").html('<a target="blank" href="https://facebook.com/'+response.id+'">https://facebook.com/'+response.id+'</a>');
                     $("#myHomeTown").text(response.hometown.name);
                     $("#myLocation").text(response.location.name);
-                     $("#myGender").text(response.gender);
+                    $("#myGender").text(response.gender);
                     $("#myProfilePic").html('<img src="https://graph.facebook.com/' + response.id + '/picture"  />');
                     $("#myProfilePicLarge").html('<img src="https://graph.facebook.com/' + response.id + '/picture?type=large"  id="myProfilePic"/>');
-                    
+     
+          }, //end of success 
+
+                           error : function(request,errorType,errorMessage){
+                                console.log(request);
+                                console.log(errorType);
+                                alert("Check your console!");
+                            },
+
+                          timeout:5000, 
+
+}); //end of ajax call     
+
+
+} //end of facebookInfo 
+$("#facebookBtn").on('click',getFacebookInfo);
+
+          
+
                     
 
                 $("#loadpost"). click(function(){
 
-                       $.ajax( {
+                      $.ajax( {
 
-                           url : "https://graph.facebook.com/me?fields=posts&access_token=" + myToken ,
+                           url : "https://graph.facebook.com/me?fields=posts&access_token=" + myFacebookToken ,
 
                                 method : "GET" ,
-                               success : function (response) {
-                                 var element = response.post.data ;
-                                  for(var i=0 ; i < 10 ; i++){
-                                   var postId = "#PostId"+i ;
-                                   $(PostId.text(response.post.data));
-                            
-                                  }
+                                success : function (response) {
+                                element = response.posts.data ;
+                                for(var i=0 ; i < 10 ; i++){
+                                var postId = "#PostId"+i ;
+                                $(postId).text(response.posts.data[i]); 
+                                }
 
                                 },
 
+                            error : function(request,errorType,errorMessage){
+                            console.log(request);
+                            console.log(errorType);
+                            alert("Check your console!");
+                            },
 
-                    }
+                             timeout:5000, // in ms
 
-                  
+               
+         
 
-                  error : function(request,errorType,errorMessage){
-                    console.log(request);
-                    console.log(errorType);
-                    alert("Check your console!");
-                },
-
-                timeout:5000, // in ms
-
-                beforeSend : function(){
-
-                    $('.loader').show();
-
-                },
-
-                complete : function(){
-
-                   $('.loader').hide();
-
-                }
-
-     }    
-
-);   
+                       }); //end of ajax call   
 
 
-} //end fb info
-} );
 
-$("#facebookBtn").on('click',getFacebookInfo);
+                 }); //end of loadpost
 
-}    );
+}); //end of document
 
